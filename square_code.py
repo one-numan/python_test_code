59a5977084d633606eefbc36e343dfe04bdc253f
one-numan <numamk786@gmail.com>
2024-03-17 15:10:50 +0530

    Add File | Square_code

---
+++ square_code.py
@@ -0,0 +1,10 @@
+def cal_square(val: int) -> int:
+    """Calculating the SQUARE
+
+    Args:
+        val (int): input Value
+
+    Returns:
+        int: Square of the Input Value
+    """
+    return val**2
